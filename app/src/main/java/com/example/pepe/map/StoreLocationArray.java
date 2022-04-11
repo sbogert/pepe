c89cfeaeb75cdcee52aa46b70d36a57441878af2
sofib <sbogert@usc.edu>
2022-04-11 15:57:55 -0700

    mid fix login

--- app/src/main/java/com/example/pepe/map/StoreLocationArray.java
+++ app/src/main/java/com/example/pepe/map/StoreLocationArray.java
@@ -1,8 +1,24 @@
 package com.example.pepe.map;
 
+import java.util.ArrayList;
 
 public class StoreLocationArray {
-    private StoreLocation storeLocArray[];
+
+    private ArrayList<StoreLocation> storeLocationArray = new ArrayList<StoreLocation>();
+
+    public StoreLocationArray (ArrayList<StoreLocation> storeLocationArray_) {
+
+        System.out.println(storeLocationArray_.get(0));
+        this.storeLocationArray = storeLocationArray_;
+    }
+
+    public void setUserProfiles(ArrayList<StoreLocation> storeLocationArray_) {
+        this.storeLocationArray = storeLocationArray_;
+    }
+
+    public ArrayList<StoreLocation> getStoreLocationArray() {
+        return storeLocationArray;
+    }
 
 
 }
